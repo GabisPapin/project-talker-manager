340881cafe682a7f8e8b2122eea2edd9f18b952d
Gabriela Papin <gabrielapapin20@gmail.com>
2022-06-20 20:40:25 -0300

    tentando fazer o req1

---
+++ helpers/readfile.js
@@ -0,0 +1,6 @@
+const { readFile } = require('fs').promises;
+
+const readTalkers = () => readFile('../talker.json', 'utf-8')
+  .then((data) => JSON.parse(data)).catch((_err) => []);
+
+module.exports = readTalkers;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const rescue = require('express-rescue');
 const bodyParser = require('body-parser');
+const { readTalkers } = require('./helpers/readfile.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +13,11 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', rescue(async (_req, res) => {
+  const data = await readTalkers();
+  res.status(HTTP_OK_STATUS).json(data);
+}));
 
 app.listen(PORT, () => {
   console.log('Online');
