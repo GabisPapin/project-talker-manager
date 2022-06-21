7a63a06633885ac3ac2abbb99df1a0ac37887230
Gabriela Papin <gabrielapapin20@gmail.com>
2022-06-21 15:06:11 -0300

    req 4 concluido

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const rescue = require('express-rescue');
+const crypto = require('crypto');
 const bodyParser = require('body-parser');
 const { readTalkers } = require('./helpers/readfile.js');
+const { isValidEmail, isValidPassword } = require('./middlewares/validations.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,6 +15,8 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use(isValidEmail, isValidPassword);
 
 app.get('/talker', rescue(async (_req, res) => {
   const data = await readTalkers();
@@ -27,6 +31,40 @@ app.get('/talker/:id', rescue(async (req, res) => {
 
   res.status(HTTP_OK_STATUS).json(talkerId);
 }));
+
+// const isValidToken = (req, res, next) => {
+//   const token = req.body.authorization;
+//   const tokenRegex = new RegExp(/^[a-zA-Z0-9]{16}$/);
+
+//   crypto.randomBytes(token, (err, buf) => {
+//     if (err) {
+//       console.log(err);
+//       return err;
+//     }
+//     console.log(buf.toString('hex'));
+//   });
+
+//   if (!tokenRegex.test(token)) {
+//     return res.status(400).json({ message: 'invalid token' });
+//   }
+
+//   next();
+// };
+
+app.post('/login', (req, res) => {
+  const token = req.body.authorization;
+  // const { email, password } = req.body;
+
+  crypto.randomBytes(8, (err, buf) => {
+    if (err) {
+      console.log(err);
+      return err;
+    }
+    console.log(buf.toString('hex'));
+  });
+
+  res.status(HTTP_OK_STATUS).json(token);
+});
 
 app.listen(PORT, () => {
   console.log('Online');
